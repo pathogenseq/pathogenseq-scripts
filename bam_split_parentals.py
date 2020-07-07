92dcd8d9ea42870aeea6d15f225f3198d14ba53c
Jody Phelan <jody.phelan@lshtm.ac.uk>
2020-07-07 22:36:03 +0100

    add vcf_parallel

--- bam_split_parentals.py
+++ bam_split_parentals.py
@@ -5,6 +5,9 @@ import random
 import subprocess
 import argparse
 
+_version = "3.0.0"
+
+
 def run_cmd(cmd,verbose=1):
     sys.stderr.write("\nRunning command:\n%s\n" % cmd)
     stdout = open("/dev/null","w")
@@ -17,10 +20,10 @@ def run_cmd(cmd,verbose=1):
 
 def main(args):
     prefix = args.bam.replace(".bam","")
-    run_cmd("samtools view -F 2048 %s.bam -h | awk '$1 ~/^@/ || ($9<2000 && $9>-2000)' | paftools.js sam2paf -L - > %s.paf" % (prefix,prefix))
-    run_cmd("paftools.js view -f maf %s.paf > %s.maf" % (prefix,prefix))
+    # run_cmd("samtools view -F 2048 %s.bam -h | awk '$1 ~/^@/ || ($9<2000 && $9>-2000)' | paftools.js sam2paf -L - > %s.paf" % (prefix,prefix))
+    # run_cmd("paftools.js view -f maf %s.paf > %s.maf" % (prefix,prefix))
     mixed_calls_dict = {}
-    run_cmd("gatk HaplotypeCaller -R %s -I %s.bam -OVI false -O /dev/stdout | bcftools view -a -M2 -m2 | bcftools query -f '%%POS\\t%%REF,%%ALT\\t[%%AD]\\n' > %s.af.txt" % (args.ref,prefix,prefix))
+    # run_cmd("gatk HaplotypeCaller -R %s -I %s.bam -OVI false -O /dev/stdout | bcftools view -a -M2 -m2 | bcftools query -f '%%POS\\t%%REF,%%ALT\\t[%%AD]\\n' > %s.af.txt" % (args.ref,prefix,prefix))
 
     for l in open("%s.af.txt" % prefix):
         row = l.rstrip().split()
@@ -56,15 +59,15 @@ def main(args):
         end_index = bisect_left(mixed_call_list,end)
         mixed_calls = mixed_call_list[start_index:end_index]
 
-
+        if read_name=="M01637:69:000000000-J2465:1:2102:9090:13026":
+            import pdb; pdb.set_trace()
         if len(mixed_calls)==0:
             other_reads.add(read_name)
         else:
 
             offset = mixed_calls[0] - start
             allele = row2[6][offset]
-            if read_name=="M01637:70:000000000-C9DH7:1:2109:14384:17446":
-                print(mixed_calls,allele)
+
             if allele not in mixed_calls_dict[mixed_calls[0]]:
                 O[random.randint(0,1)].write(read_name+"\n")
             elif mixed_calls_dict[mixed_calls[0]][allele]<0.5:
@@ -86,10 +89,11 @@ def main(args):
     run_cmd("gatk FilterSamReads -I %s.bam -O %s.major.bam --FILTER includeReadList --READ_LIST_FILE %s.major.txt" % (prefix,prefix,prefix))
     run_cmd("gatk FilterSamReads -I %s.bam -O %s.minor.bam --FILTER includeReadList --READ_LIST_FILE %s.minor.txt" % (prefix,prefix,prefix))
 
-parser = argparse.ArgumentParser(description='TBProfiler pipeline',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser = argparse.ArgumentParser(description='Bam splitting pipeline',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--bam',type=str,help='The bam file you would like to split',required=True)
 parser.add_argument('--ref',type=str,help='The reference file',required=True)
 #parser.add_argument('--no-clean',action="store_true",help='Don\'t clean up temp files')
+parser.add_argument('--version', action='version', version=_version)
 parser.set_defaults(func=main)
 
 args = parser.parse_args()
